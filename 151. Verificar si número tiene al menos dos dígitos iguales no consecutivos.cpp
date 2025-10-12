d8d7388bd1f2b49149c5c9f3338495518ccb3aef
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-12 08:15:39 -0500

    151. Verificar si número tiene al menos dos dígitos iguales no consecutivos.cpp
    Verifica frecuencia sin orden.

--- 151. Verificar si número tiene al menos dos dígitos iguales no consecutivos.cpp
+++ 151. Verificar si número tiene al menos dos dígitos iguales no consecutivos.cpp
@@ -1,1 +1,21 @@
+#include 
+using namespace std;
+
+int main() {
+int n, freq[10] = {0};
+cin >> n;
+while (n != 0) {
+freq[n % 10]++;
+n /= 10;
+}
+bool cumple = false;
+for (int i = 0; i < 10; i++) {
+if (freq[i] >= 2) {
+cumple = true;
+break;
+}
+}
+cout << (cumple ? "Tiene dígitos iguales no consecutivos" : "No tiene");
+return 0;
+}
 
