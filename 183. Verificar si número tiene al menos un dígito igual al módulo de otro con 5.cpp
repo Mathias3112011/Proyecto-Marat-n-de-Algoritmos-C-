e735a7f27529be90a0dbfe8874229b185c20663f
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-12 16:22:23 -0500

    183. Verificar si número tiene al menos un dígito igual al módulo de otro con 5.cpp
    Busca relación con módulo 5.

---
+++ 183. Verificar si número tiene al menos un dígito igual al módulo de otro con 5.cpp
@@ -0,0 +1,19 @@
+#include 
+using namespace std;
+
+int main() {
+int n, d[10], c = 0;
+cin >> n;
+while (n != 0) {
+d[c++] = n % 10;
+n /= 10;
+}
+bool cumple = false;
+for (int i = 0; i < c; i++)
+for (int j = 0; j < c; j++)
+if (i != j && d[i] == d[j] % 5)
+cumple = true;
+cout << (cumple ? "Cumple condición" : "No cumple");
+return 0;
+}
+
