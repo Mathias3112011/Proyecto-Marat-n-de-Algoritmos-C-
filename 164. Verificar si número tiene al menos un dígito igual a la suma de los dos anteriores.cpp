5273e5f83cd3c6f76ddf7fb616b148672824462b
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-12 14:16:37 -0500

    164. Verificar si número tiene al menos un dígito igual a la suma de los dos anteriores.cpp
    Verifica suma secuencial.

---
+++ 164. Verificar si número tiene al menos un dígito igual a la suma de los dos anteriores.cpp
@@ -0,0 +1,21 @@
+#include 
+using namespace std;
+
+int main() {
+int n, d1 = -1, d2 = -1;
+bool cumple = false;
+cin >> n;
+while (n != 0) {
+int d = n % 10;
+if (d1 != -1 && d2 != -1 && d == d1 + d2) {
+cumple = true;
+break;
+}
+d2 = d1;
+d1 = d;
+n /= 10;
+}
+cout << (cumple ? "Dígito igual a suma de dos anteriores" : "No cumple");
+return 0;
+}
+
