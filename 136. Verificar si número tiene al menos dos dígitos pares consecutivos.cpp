506c31ef143c1f51bc9f6f0b8366e05a47ad253d
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-12 07:22:54 -0500

    136. Verificar si número tiene al menos dos dígitos pares consecutivos.cpp
     Verifica pares consecutivos.

---
+++ 136. Verificar si número tiene al menos dos dígitos pares consecutivos.cpp
@@ -0,0 +1,19 @@
+#include 
+using namespace std;
+
+int main() {
+int n, prev = -1;
+bool consecutivos = false;
+cin >> n;
+while (n != 0) {
+int d = n % 10;
+if (d % 2 == 0 && prev % 2 == 0) {
+consecutivos = true;
+break;
+}
+prev = d;
+n /= 10;
+}
+cout << (consecutivos ? "Tiene pares consecutivos" : "No tiene");
+return 0;
+}
