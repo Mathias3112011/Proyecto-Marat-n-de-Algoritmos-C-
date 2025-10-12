ea220084eaf3f03795cc926c232dd8ea54918547
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-12 14:06:56 -0500

    159. Verificar si número tiene al menos un dígito igual al producto de sus vecinos.cpp
    Verifica multiplicación entre vecinos.

---
+++ 159. Verificar si número tiene al menos un dígito igual al producto de sus vecinos.cpp
@@ -0,0 +1,21 @@
+#include 
+using namespace std;
+
+int main() {
+int n;
+int d1, d2, d3;
+bool cumple = false;
+cin >> n;
+while (n >= 100) {
+d1 = n % 10;
+d2 = (n / 10) % 10;
+d3 = (n / 100) % 10;
+if (d2 == d1 * d3) {
+cumple = true;
+break;
+}
+n /= 10;
+}
+cout << (cumple ? "Dígito igual a producto de vecinos" : "No cumple");
+return 0;
+}
