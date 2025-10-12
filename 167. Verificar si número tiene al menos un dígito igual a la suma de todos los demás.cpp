9637739a5871b22b5b8896fe9ed08d0f616a4150
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-12 14:21:14 -0500

    167. Verificar si número tiene al menos un dígito igual a la suma de todos los demás.cpp
    Verifica si un dígito es igual a la suma de los demás.

---
+++ 167. Verificar si número tiene al menos un dígito igual a la suma de todos los demás.cpp
@@ -0,0 +1,24 @@
+#include 
+using namespace std;
+
+int main() {
+int n, temp, suma = 0, cumple = 0;
+cin >> n;
+temp = n;
+while (temp != 0) {
+suma += temp % 10;
+temp /= 10;
+}
+temp = n;
+while (temp != 0) {
+int d = temp % 10;
+if (d == suma - d) {
+cumple = 1;
+break;
+}
+temp /= 10;
+}
+cout << (cumple ? "Cumple condición" : "No cumple");
+return 0;
+}
+
