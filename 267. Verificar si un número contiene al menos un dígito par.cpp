fffbbf968561ec8446ec69acfdbf5c25c609d227
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-12 22:17:21 -0500

    267. Verificar si un número contiene al menos un dígito par.cpp
    Busca al menos un dígito par.

---
+++ 267. Verificar si un número contiene al menos un dígito par.cpp
@@ -0,0 +1,17 @@
+#include 
+using namespace std;
+
+int main() {
+int n;
+bool cumple = false;
+cin >> n;
+while (n != 0) {
+if ((n % 10) % 2 == 0) {
+cumple = true;
+break;
+}
+n /= 10;
+}
+cout << (cumple ? "Contiene dígito par" : "No contiene");
+return 0;
+}
