5e58b201a7980b0e0ca59df97e5ea6db2cf19e4e
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-12 22:55:22 -0500

    293. Verificar si un número contiene al menos un dígito igual al producto de dos distintos.cpp
    Busca multiplicación entre pares de dígitos.

---
+++ 293. Verificar si un número contiene al menos un dígito igual al producto de dos distintos.cpp
@@ -0,0 +1,19 @@
+#include 
+using namespace std;
+
+int main() {
+int n, d[10], c = 0;
+cin >> n;
+while (n != 0) {
+d[c++] = n % 10;
+n /= 10;
+}
+bool cumple = false;
+for (int i = 0; i < c; i++)
+for (int j = 0; j < c; j++)
+for (int k = 0; k < c; k++)
+if (i != j && i != k && j != k && d[i] == d[j] * d[k])
+cumple = true;
+cout << (cumple ? "Cumple condición" : "No cumple");
+return 0;
+}
