173a1ba3b54bd43ca9bcce2a6b29bd50af48a71a
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-12 22:22:02 -0500

    269. Verificar si un número contiene al menos un dígito mayor que 5.cpp
    Busca dígitos mayores que 5.

--- 269. Verificar si un número contiene al menos un dígito mayor que 5.cpp
+++ 269. Verificar si un número contiene al menos un dígito mayor que 5.cpp
@@ -1,1 +1,19 @@
+#include 
+using namespace std;
+
+int main() {
+int n;
+bool cumple = false;
+cin >> n;
+while (n != 0) {
+if (n % 10 > 5) {
+cumple = true;
+break;
+}
+n /= 10;
+}
+cout << (cumple ? "Contiene dígito > 5" : "No contiene");
+return 0;
+}
+
 
