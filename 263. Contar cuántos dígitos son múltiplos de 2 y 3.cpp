7cac61431bd5dedf0b65f62492a66bbbbda01436
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-12 22:13:56 -0500

    263. Contar cuántos dígitos son múltiplos de 2 y 3.cpp
    Dígitos múltiplos de 6.

---
+++ 263. Contar cuántos dígitos son múltiplos de 2 y 3.cpp
@@ -0,0 +1,14 @@
+#include 
+using namespace std;
+
+int main() {
+int n, count = 0;
+cin >> n;
+while (n != 0) {
+int d = n % 10;
+if (d % 2 == 0 && d % 3 == 0) count++;
+n /= 10;
+}
+cout << count;
+return 0;
+}
