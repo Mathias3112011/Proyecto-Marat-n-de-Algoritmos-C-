521ac7b93c1128784e0a4f4f31150f5e0677504f
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-12 22:26:26 -0500

    273. Verificar si un número contiene al menos un dígito igual a 1.cpp
    Busca dígito igual a 1.

---
+++ 273. Verificar si un número contiene al menos un dígito igual a 1.cpp
@@ -0,0 +1,17 @@
+#include 
+using namespace std;
+
+int main() {
+int n;
+bool cumple = false;
+cin >> n;
+while (n != 0) {
+if (n % 10 == 1) {
+cumple = true;
+break;
+}
+n /= 10;
+}
+cout << (cumple ? "Contiene dígito 1" : "No contiene");
+return 0;
+}
