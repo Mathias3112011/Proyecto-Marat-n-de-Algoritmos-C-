6ef2338b0874d82527b6ac61ae35f2ff4ad9a133
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-12 21:48:13 -0500

    242. Contar cuántos dígitos distintos tiene un número.cpp
    Dígitos únicos.

---
+++ 242. Contar cuántos dígitos distintos tiene un número.cpp
@@ -0,0 +1,15 @@
+#include 
+using namespace std;
+
+int main() {
+int n, freq[10] = {0}, count = 0;
+cin >> n;
+while (n != 0) {
+freq[n % 10]++;
+n /= 10;
+}
+for (int i = 0; i < 10; i++)
+if (freq[i] > 0) count++;
+cout << count;
+return 0;
+}
