549dac78742430513b9996c9ff4a945df321306b
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-12 22:04:41 -0500

    256. Sumar los dígitos distintos.cpp
    Suma de dígitos únicos.

---
+++ 256. Sumar los dígitos distintos.cpp
@@ -0,0 +1,16 @@
+#include 
+using namespace std;
+
+int main() {
+int n, freq[10] = {0}, suma = 0;
+cin >> n;
+while (n != 0) {
+int d = n % 10;
+freq[d]++;
+n /= 10;
+}
+for (int i = 0; i < 10; i++)
+if (freq[i] == 1) suma += i;
+cout << suma;
+return 0;
+}
