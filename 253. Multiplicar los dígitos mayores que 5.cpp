c2e2069ff0387bda3c57484f2142d7446aa0d928
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-12 21:59:07 -0500

    253. Multiplicar los dígitos mayores que 5.cpp
    Producto de dígitos > 5.

---
+++ 253. Multiplicar los dígitos mayores que 5.cpp
@@ -0,0 +1,18 @@
+#include 
+using namespace std;
+
+int main() {
+int n, prod = 1;
+bool tiene = false;
+cin >> n;
+while (n != 0) {
+int d = n % 10;
+if (d > 5) {
+prod *= d;
+tiene = true;
+}
+n /= 10;
+}
+cout << (tiene ? prod : 0);
+return 0;
+}
