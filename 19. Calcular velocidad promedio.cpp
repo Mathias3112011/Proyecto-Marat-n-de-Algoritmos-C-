db88c146c9e5d619c18c2ef588dc7e09a487fd25
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-20 15:21:13 -0500

    19. Calcular velocidad promedio.cpp

---
+++ 19. Calcular velocidad promedio.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+double distancia, tiempo;
+cin >> distancia >> tiempo;
+cout << distancia / tiempo;
+return 0;
+}
+20. Mostrar inicial y apellido
+#include <iostream>
+using namespace std;
+int main() {
+char inicial;
+string apellido;
+cin >> inicial >> apellido;
+cout << inicial << ". " << apellido;
+return 0;
+}
