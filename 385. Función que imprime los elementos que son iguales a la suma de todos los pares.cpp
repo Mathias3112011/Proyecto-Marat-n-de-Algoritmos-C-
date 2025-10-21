0eb8c3601a819cce53b1ccedc2d466ca769c737a
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-20 22:18:56 -0500

    385. Función que imprime los elementos que son iguales a la suma de todos los pares.cpp
    Coinciden con suma de pares.

---
+++ 385. Función que imprime los elementos que son iguales a la suma de todos los pares.cpp
@@ -0,0 +1,10 @@
+#include
+using namespace std;
+void imprimirIgualesSumaPares(int arr[], int n) {
+int suma = 0;
+for (int i = 0; i < n; i++)
+if (arr[i] % 2 == 0) suma += arr[i];
+for (int i = 0; i < n; i++)
+if (arr[i] == suma)
+cout << arr[i] << " ";
+}
