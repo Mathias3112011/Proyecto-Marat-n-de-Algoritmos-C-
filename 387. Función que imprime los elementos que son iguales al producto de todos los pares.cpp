f74340405b33c2ca99c608f2eed937b92887a25c
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-20 22:24:05 -0500

    387. Función que imprime los elementos que son iguales al producto de todos los pares.cpp
    Coinciden con producto de pares.

---
+++ 387. Función que imprime los elementos que son iguales al producto de todos los pares.cpp
@@ -0,0 +1,15 @@
+#include
+using namespace std;
+void imprimirIgualesProductoPares(int arr[], int n) {
+int prod = 1;
+bool tiene = false;
+for (int i = 0; i < n; i++)
+if (arr[i] % 2 == 0) {
+prod *= arr[i];
+tiene = true;
+}
+if (!tiene) return;
+for (int i = 0; i < n; i++)
+if (arr[i] == prod)
+cout << arr[i] << " ";
+}
