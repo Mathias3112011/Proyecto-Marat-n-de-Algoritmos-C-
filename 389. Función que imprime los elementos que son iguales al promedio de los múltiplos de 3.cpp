7d4fdd37234faafd21aee3ce0b94b8c176fc7eb5
Mathias3112011 <mquinde@alumno.montepiedra.edu.ec>
2025-10-20 22:27:34 -0500

    389. Función que imprime los elementos que son iguales al promedio de los múltiplos de 3.cpp
    Coinciden con promedio de múltiplos de 3.

---
+++ 389. Función que imprime los elementos que son iguales al promedio de los múltiplos de 3.cpp
@@ -0,0 +1,15 @@
+#include
+using namespace std;
+void imprimirPromedioMultiplos3(int arr[], int n) {
+int suma = 0, count = 0;
+for (int i = 0; i < n; i++)
+if (arr[i] % 3 == 0) {
+suma += arr[i];
+count++;
+}
+if (count == 0) return;
+int prom = suma / count;
+for (int i = 0; i < n; i++)
+if (arr[i] == prom)
+cout << arr[i] << " ";
+}
